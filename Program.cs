789f75564aad8cd14981f38ad4d0948ac18932ce
Almos <Almos@ALMOS-PC>
2022-11-18 19:12:25 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace HeimdallUpdate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heimdall Updater v1.0");
+
+            // Get the current path
+            string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        }
+    }
+}
